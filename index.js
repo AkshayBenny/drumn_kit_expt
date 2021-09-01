31763c5ed56f9fd41a3c51d8de249ed4f1bd71f6
AkshayBenny <akshaybennygh@gmail.com>
2021-09-01 09:45:10 +0530

    js update

--- index.js
+++ index.js
@@ -1,49 +1,62 @@
-for (var i = 0; i<document.querySelectorAll('button').length; i++) {
+var buttonNumber = document.querySelectorAll('.drum').length;
+
+for (var i = 0; i< buttonNumber ; i++) {
   document.querySelectorAll('.drum')[i].addEventListener('click', function() {
     
-
     var buttonInnerHTML = this.innerHTML;
-    switch(buttonInnerHTML){
-
-      case 'w':
-        var audio = new Audio('sounds/tom-1.mp3');
-        audio.play();
-      break;
-
-      case 'a':
-        var audio = new Audio('sounds/tom-2.mp3');
-        audio.play();
-      break;
-
-      case 's':
-        var audio = new Audio('sounds/tom-3.mp3');
-        audio.play();
-      break;
-      
-      case 'd':
-        var audio = new Audio('sounds/tom-4.mp3');
-        audio.play();
-      break;
-
-      case 'j':
-        var audio = new Audio('sounds/crash.mp3');
-        audio.play();
-      break;
-
-      case 'k':
-        var audio = new Audio('sounds/kick-bass.mp3');
-        audio.play();
-      break;
-
-      case 'l':
-        var audio = new Audio('sounds/snare.mp3');
-        audio.play();
-      break;
-
-      default:
-    }
-      
-    
+    makeSound(buttonInnerHTML); 
 
   });
+  
+}
+
+
+document.addEventListener('keypress', function(event){
+  makeSound(event.key);
+})
+
+
+function makeSound(key) {
+
+
+
+  switch(key) {
+
+    case 'w':
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+    break;
+
+    case 'a':
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+    break;
+
+    case 's':
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+    break;
+    
+    case 'd':
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+    break;
+
+    case 'j':
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+    break;
+
+    case 'k':
+      var kick_bass = new Audio('sounds/kick-bass.mp3');
+      kick_bass.play();
+    break;
+
+    case 'l':
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+    break;
+
+    default: alert('Only keys w, a, s, d, j, k, l are assigned audio.')
+  }
 }
